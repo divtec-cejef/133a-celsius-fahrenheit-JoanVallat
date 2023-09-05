c4af1bbed6d3e5aca4e7b272ebd2e655d1abf046
valljoa <joan.vallat@divtec.ch>
2023-09-05 14:59:23 +0200

    Exercice farenheit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 /**
  * Fichier de base pour application web HTML/CSS/JS
- * @author  Steve Fallet
+ * @author  Joan Vallat
  * @version 0.2
  * @since   2019-08-19
  */
 'use strict'; // Demande un interprétation stricte du code
 
-let a = 'Bonjour';
 
-console.log(a);
+let degreCelcius = parseInt(prompt('Température en °C :'));
+
+if(isNaN(degreCelcius)){
+    alert('Veuillez entrer un nombre !');
+}else{
+    let degreFarenheit = degreCelcius * 9 / 5 + 32;
+    prompt(`${degreCelcius}°C = ${degreFarenheit}°F`);
+}
+
+
+//[°F] = [°C] x 9 / 5 + 32
