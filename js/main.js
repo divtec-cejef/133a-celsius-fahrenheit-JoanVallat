ceaa7cc836dc0dc15159f2762f01ecd68d3063a3
valljoa <joan.vallat@divtec.ch>
2023-09-05 15:00:42 +0200

    Commentaire du code

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@
  */
 'use strict'; // Demande un interprétation stricte du code
 
-
+//Demande à l'utilisateur d'entrer un nombre
 let degreCelcius = parseInt(prompt('Température en °C :'));
 
+//test si la veleur est un nombre
 if(isNaN(degreCelcius)){
+    //Si non affiche un message d'erreur
     alert('Veuillez entrer un nombre !');
 }else{
+    //si oui affiche le résultat
     let degreFarenheit = degreCelcius * 9 / 5 + 32;
     prompt(`${degreCelcius}°C = ${degreFarenheit}°F`);
 }
 
-
+//Formule
 //[°F] = [°C] x 9 / 5 + 32
